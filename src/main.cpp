5281cfe33df4146b6121c0cf01b1e9ef36e07d84
ElPatatone <rahman.hm2002@gmail.com>
2024-01-05 20:39:38 +0000

    feat: can now parse and store the set, reps, weight information

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,17 @@
 #include <sstream>
 #include <vector>
 
+struct Exercise {
+    std::string name;
+    int set;
+    int reps;
+    std::string weight;
+};
+
 struct Workout {
 
     std::string date;
-    std::string training;
+    std::vector<Exercise> exercises;
     std::string start_time;
     std::string location;
     int duration;
@@ -15,30 +22,37 @@ struct Workout {
 
 };
 
-struct ExerciseSet {
-    std::string exercise;
-    int sets;
-    int reps;
-    int weight;
-    std::string unit;
-};
-
 int main (int argc, char *argv[]) {
 
     std::ifstream file{"example.txt"};
 
-    if (!file) {
+    if (!file.is_open()) {
         std::cerr << "File could not be opened" << std::endl;
         return 1;
     }
 
-    Workout workout;
+    Workout currentWorkout;
+    std::string line;
 
-    while (file) {
-        std::string line;
-        std::getline(file, line);
-        std::cout << line << "\n";
+    while (getline(file, line)) {
+        // Check for date line
+        if (line.find("/") != std::string::npos) {
+            currentWorkout.date = line;
+        } else if (line.find("@") != std::string::npos) {
+            std::istringstream stream{line};
+            Exercise exercise;
+            char x;
+            char at;
+            stream >> exercise.set >> x >> exercise.reps >> at >> exercise.weight;
+            currentWorkout.exercises.push_back(exercise);
+        }
     }
+
+    for (const Exercise& exercise: currentWorkout.exercises) {
+        std::cout << "Set: " << exercise.set << ", Reps: " << exercise.reps << ", Weight: " << exercise.weight << std::endl;
+    }
+
+    file.close();
 
     return 0;
 }
