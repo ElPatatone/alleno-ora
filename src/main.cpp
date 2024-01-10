69ebeea8fd9b54cd4a0507aca1a43ebba9c9af2b
ElPatatone <rahman.hm2002@gmail.com>
2024-01-10 01:16:43 +0000

    feat: improve the parsing of information + better code structure

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,26 @@
 #include <sstream>
 #include <vector>
 
+struct Set {
+    std::string setType;
+    int setNumber;
+    int repsNumber;
+    int weight;
+};
+
 struct Exercise {
     std::string name;
-    int set;
-    int reps;
-    std::string weight;
+    std::vector<Set> setsVector;
 };
 
 struct Workout {
-
+    std::vector<Exercise> exercisesVector;
     std::string date;
-    std::vector<Exercise> exercises;
-    std::string start_time;
+    std::string startTime;
     std::string location;
     int duration;
     int rating;
-
 };
-
-
 
 int main (int argc, char *argv[]) {
 
@@ -33,33 +34,66 @@ int main (int argc, char *argv[]) {
         return 1;
     }
 
-    Workout currentWorkout;
     std::string line;
 
+    Workout workout;
+    Exercise exercise;
+    Set set;
+
     while (getline(file, line)) {
-        // Check for date line
-        Exercise exercise;
-        if (line.find("/") != std::string::npos) {
-            currentWorkout.date = line;
+        if (line.empty()) {
+            continue;
         }
-        else if (line.find("-") != std::string::npos){
-            exercise.name = line;
-            currentWorkout.exercises.push_back(exercise);
+        // Check for date
+        else if (line.find("Date") != std::string::npos) {
+            workout.date = line;
         }
+        // Check for start time
+        else if (line.find("Start") != std::string::npos) {
+            workout.startTime = line;
+        }
+        // Check for duration
+        else if (line.find("Duration") != std::string::npos) {
+            workout.duration = std::stoi(line.substr(10));
+        }
+        // Check for location
+        else if (line.find("Location") != std::string::npos) {
+            workout.location = line;
+        }
+        // Check for rating
+        else if (line.find("Rating") != std::string::npos) {
+            workout.rating = std::stoi(line.substr(8));
+        }
+        // Check for exercise name
+        else if (line[0] == '-'){
+            if (!exercise.name.empty()) {
+                workout.exercisesVector.push_back(exercise);
+            }
+            exercise = Exercise();
+            exercise.name = line.substr(2);
+        }
+        // Check for set, reps, weight
         else if (line.find("@") != std::string::npos) {
             std::istringstream stream{line};
             char token;
-            stream >> exercise.set >> token >> exercise.reps >> token >> exercise.weight;
-            currentWorkout.exercises.push_back(exercise);
+            stream >> set.setNumber >> token >> set.repsNumber >> token >> set.weight;
+            exercise.setsVector.push_back(set);
         }
-
     }
 
-    for (const Exercise& exercise : currentWorkout.exercises) {
-        std::cout << "Set: " << exercise.set << ", Reps: " << exercise.reps << ", Weight: " << exercise.weight << std::endl;
+    // add last exercise being parsed
+    if (!exercise.name.empty()) {
+        workout.exercisesVector.push_back(exercise);
     }
 
     file.close();
+
+    for (const auto& exercise: workout.exercisesVector) {
+        std::cout << exercise.name << std::endl;
+        for (const auto& set : exercise.setsVector) {
+            std::cout << set.setNumber << " " << set.repsNumber << " "  << set.weight << std::endl;
+        }
+    }
 
     return 0;
 }
